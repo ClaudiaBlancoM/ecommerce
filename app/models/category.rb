057ccbed55c975f480dc333b8ac9386d44034118
ClaudiaBlancoM <cabm0626@gmail.com>
2022-05-30 23:17:44 -0400

    feat: Join to product_category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,3 @@
 class Category < ApplicationRecord
-end
+    has_and_belongs_to_many :products
+  end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
-end
+    # Relationships
+    has_and_belongs_to_many :categories
+  end
